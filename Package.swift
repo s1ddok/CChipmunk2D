fc67f79ace3d4e10041bcbe280aa09aae1a3568a
s1ddok <siddok@gmail.com>
2017-06-25 22:55:30 +0300

    Migrate to Manifest v4 API

--- Package.swift
+++ Package.swift
@@ -1,5 +1,22 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "CChipmunk2D"
+    name: "CChipmunk2D",
+    products: [
+        .library(
+            name: "CChipmunk2D",
+            type: .static,
+            targets: ["CChipmunk2D"]),
+        ],
+    targets: [
+        .target(
+            name: "CChipmunk2D",
+            path: ".",
+            sources: ["CChipmunk2D"],
+            publicHeadersPath: "CChipmunk2D/include"
+        )
+    ]
 )
+
+
